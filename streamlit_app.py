73c1b7de12a3f8b69c19351354301a719f3c0eef
Mehmood-Ul-Hassan <mehmoodhassan812959@gmail.com>
2026-08-19 19:10:27 +0500

    Add SEARCH_ON column to fruit options query

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write("The name on your smoothie will be:", Name_on_order)
 cnx =st.connection("snowflake")
 session=cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe,use_container_width=True)
 #st.stop
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
@@ -29,8 +29,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+ ' '
-        
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + 'Nutrition Information')
